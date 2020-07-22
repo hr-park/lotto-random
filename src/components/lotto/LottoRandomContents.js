5e7e6d5917007b570601ccdcf090e3757db421c1
hr-park <ever-iver@hanmail.net>
2020-07-22 11:26:38 +0900

    레이아웃 완성

---
+++ src/components/lotto/LottoRandomContents.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './LottoRandomContents.css';
+import LottoBoxComponent from './lotto-box/LottoBoxComponent';
+
+class LottoRandomContents extends Component {
+    render() {
+        const {lottoNumbers} = this.props;
+
+        return(
+            <div className="lotto-random-contents">
+                {lottoNumbers.map((lottoNumber, i) =>
+                <LottoBoxComponent
+                    key={i}
+                    lottoNumber={lottoNumber}
+                />)}
+            </div>
+        );
+    }
+}
+export default LottoRandomContents;
